d0d62e2ac7afc943ff20378c5cd7500466a4ba43
DanielOrtiz05 <you@example.com>
2023-08-13 22:14:14 -0500

    Set-add

--- src/es6/09-generators.js
+++ src/es6/09-generators.js
@@ -10,3 +10,4 @@ console.log(array.next().value);
 console.log(array.next().value);
 console.log(array.next().value);
 console.log(array.next().value);
+
---
+++ src/es6/10-set-add.js
@@ -0,0 +1,5 @@
+let list = new Set();
+
+list.add("Item-1").add("Item-2").add("Item-3");
+
+console.log(list);
